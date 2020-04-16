57b96de9d327285f7504aab1a18df71d10e1b3d6
Lanzhou Jiang <lzjiang99@gmail.com>
2020-04-16 21:00:51 +1000

    Seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# puts "destroying all of the toys records!!"
+# Toy.destroy_all
+# puts "toys successfully deleted!"
+
+puts "toys seeding"
+20.times do
+  Toy.create(
+    name:Faker::Food.fruits,
+    description: Faker::Color.color_name,
+    date: Faker::Date.between(from: 5.years.ago, to: Date.today),
+    user: Faker::FunnyName.name_with_initial
+    )
+  end
+
+  puts "20 toys seeded"
+  puts "All done"
